506c2114d107b9b00cf5dae98125a5c5099ee0b8
immedha <immedha@outlook.com>
2025-03-01 18:17:39 -0800

    Did background image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,11 @@ import { FC } from 'react';
 
 const Header: FC = () => {
   return (
-    <header className="p-4 text-xl font-bold text-center border-b">
-      Scrolla
+    <header className="flex items-center p-4 text-4xl text-white righteous-regular">
+      <img src="logo.png" alt="Logo" className="mr-2 inline-block" style={{ height: '1em' }} />
+      <p className="inline-block">Scrolla</p>
     </header>
   );
 };
 
-export default Header; 
+export default Header;
